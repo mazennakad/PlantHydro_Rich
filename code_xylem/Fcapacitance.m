1469349424f9d5992a47b01ee7eccbb574569301
TonySalamoun <142812136+TonySalamoun@users.noreply.github.com>
2025-04-09 15:25:14 +0300

    Delete code_xylem/Fcapacitance.m

--- code_xylem/Fcapacitance.m
+++
@@ -1,8 +0,0 @@
-function C = Fcapacitance(psi,thetasat,psi0,Az,p)
-%thetasat water content at saturation
-%psi0 empirical paramter for water pressure of dry xylem
-%Az cross-sectional area for a xylem
-%p empirical curve fitting coefficient for water content
-C=Az.*p.*thetasat.*((psi0-psi)./psi0).^(-(p+1));
-
- 
