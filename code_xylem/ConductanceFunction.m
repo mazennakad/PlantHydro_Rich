1ed80180badf7bf2363daf7ddf1a0b9a3c5a97e7
TonySalamoun <tonyjunior.salamoun@gmail.com>
2025-04-15 16:20:16 +0300

    Added units after I made sure of them, fixed a small mistake in conductance function, did not solve the probem

--- code_xylem/ConductanceFunction.m
+++ code_xylem/ConductanceFunction.m
@@ -7,5 +7,5 @@ function K=ConductanceFunction(psi,Az,Am,ED,kmax,c1,c2)
 %c1:empirical curve fitting coefficient-cavitation pressure
 %c2:empirical curve fitting coefficent for conductance
 Aaz=(Az/Am).^((ED-2)/2).*Az;   %Effective cross-sectional area
-K=Aaz.*kmax.^exp(-(-psi./c1).^c2);
+K=Aaz.*kmax.*exp(-(-psi./c1).^c2);
 
